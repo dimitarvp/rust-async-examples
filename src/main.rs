f4e82b0fcdb177f31568980c7bcae691a31d3f24
Dimitar Panayotov <mitko.p@gmail.com>
2022-08-28 04:00:58 +0300

    demo out-of-order execution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use env_logger::fmt::TimestampPrecision;
+use env_logger::Env;
+use futures::select;
+use futures::stream::{FuturesUnordered, StreamExt};
+use log::trace;
+use tokio::time::{sleep, Duration, Instant};
+
+async fn worker(i: u64) -> u64 {
+    let time = Instant::now();
+    sleep(Duration::from_millis(100 / i)).await;
+    trace!("Worker {} elapsed: {:?}", i, time.elapsed());
+    i
+}
+
+fn init() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace"))
+        .format_timestamp(Some(TimestampPrecision::Nanos))
+        .init();
+}
+
+#[tokio::main]
+async fn main() {
+    init();
+
+    let time = Instant::now();
+
+    // This program demonstrates executing async workers out of their original order
+    // because they take different amounts of real time to finish.
+
+    let mut tasks: FuturesUnordered<_> = (1..=5).map(|x| async move { worker(x).await }).collect();
+    let mut completed = 0;
+    loop {
+        select! {
+            _num = tasks.select_next_some() => {
+                completed += 1;
+            },
+            complete => break,
+        }
+    }
+
+    trace!("Completed {} workers", completed);
+    trace!("Program took {:?}", time.elapsed());
 }
