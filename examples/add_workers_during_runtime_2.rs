8b26356d6e207cc02ecf275fd86c2d2b0aa3d281
Dimitar Panayotov <mitko.p@gmail.com>
2022-09-03 21:02:58 +0300

    use proper `tokio` constructs

--- examples/add_workers_during_runtime_2.rs
+++ examples/add_workers_during_runtime_2.rs
@@ -1,8 +1,6 @@
 use env_logger::fmt::TimestampPrecision;
 use env_logger::Env;
-use futures::stream::select_all;
-use futures::stream::FuturesUnordered;
-use futures::stream::StreamExt;
+use futures::stream::{self, FuturesUnordered, SelectAll, StreamExt};
 use log::{error, trace};
 use tokio::sync::mpsc;
 use tokio::time::{sleep, Duration, Instant};
@@ -15,49 +13,37 @@ fn init_logging() {
 }
 
 async fn exercise_add_workers_during_runtime() {
-    // Make a shared reference to a list of futures and fill them up
-    // while a main receiver loop is already trying to wait on them.
-
-    let mut stream_producers: FuturesUnordered<_> = [("A", 2), ("B", 3), ("C", 4)]
+    let stream_producers: FuturesUnordered<_> = [("A", 2), ("B", 3), ("C", 4)]
         .map(|(name, i)| {
-            // We don't seem to need `tokio::spawn` (namely true parallelism) here
-            // because the different tasks yield at different times.
+            let (tx, rx) = mpsc::unbounded_channel::<String>();
+
             tokio::spawn(async move {
-                let (tx, rx) = mpsc::unbounded_channel::<String>();
                 sleep(Duration::from_millis(480 / i)).await;
 
-                async move {
-                    for _ in 1..=i {
-                        sleep(Duration::from_millis(120 / i)).await;
-                        let to_send = format!("{}_{}", name, i);
-                        match tx.send(to_send.clone()) {
-                            Ok(_) => trace!("Sent {}", to_send.clone()),
-                            Err(e) => error!("{}", format!("{:?}", e)),
-                        }
+                for _ in 1..=i {
+                    sleep(Duration::from_millis(120 / i)).await;
+                    let to_send = format!("{}_{}", name, i);
+                    match tx.send(to_send.clone()) {
+                        Ok(_) => trace!("Sent {}", to_send.clone()),
+                        Err(e) => error!("{}", format!("{:?}", e)),
                     }
                 }
-                .await;
+            });
 
-                ((name, i), UnboundedReceiverStream::new(rx))
-            })
+            ((name, i), UnboundedReceiverStream::new(rx))
         })
         .into_iter()
         .collect();
 
-    let mut streams = select_all(FuturesUnordered::new());
+    let mut stream_producers = stream::iter(stream_producers).fuse();
+
+    let mut streams = SelectAll::new();
 
     loop {
         futures::select! {
-            stream_result = stream_producers.select_next_some() => {
-                match stream_result {
-                    Ok(((name, i), stream)) => {
-                        trace!("Produced stream {}_{}", name, i);
-                        streams.push(stream);
-                    },
-                    Err(e) => {
-                        error!("Could not get stream: {:?}", e);
-                    }
-                }
+            ((name, i), stream) = stream_producers.select_next_some() => {
+                trace!("Produced stream {}_{}", name, i);
+                streams.push(stream);
             },
             value = streams.select_next_some() => {
                 trace!("Received {}", value);
